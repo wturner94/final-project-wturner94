fd387c6d946f325c6d0369b2eccf2ee5ac75a046
Turner <WTurner@tegna.com>
2023-11-09 14:32:58 -0500

    limited the parameters of the dataframe to the 3 pertinent columns

--- televisionRatings.py
+++ televisionRatings.py
@@ -1,9 +1,14 @@
 # read-in datasets from CSVs
-
 import pandas as pd
-monday_ratings = pd.read_excel('10_30_2023_Monday_RATINGS_Preliminary.xlsx', index_col=[2])
 
-print(monday_ratings.iloc[4:-2,2:8])
+monday_ratings = pd.read_excel('10_30_2023_Monday_RATINGS_Preliminary.xlsx', skipfooter=2)
+monday_ratings = monday_ratings[['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4']]
+monday_ratings = monday_ratings.dropna()
+
+
+
+# print(monday_ratings.iloc[4:-2,2:8])
+print(monday_ratings)
 
 # need to set column titles
 # find more xlsx from Mondays near that date and download
