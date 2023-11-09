0dedf5f9edc814ca4d9aa9ff58b00b2de2a51fd7
Turner <WTurner@tegna.com>
2023-11-09 14:41:03 -0500

    annotated/cleaned up existing commands

--- televisionRatings.py
+++ televisionRatings.py
@@ -1,18 +1,13 @@
-# read-in datasets from CSVs
 import pandas as pd
 
+# read-in datasets from XLSXs
 monday_ratings = pd.read_excel('10_30_2023_Monday_RATINGS_Preliminary.xlsx', skipfooter=2)
+# constrain dataframe to the 3 pertinent columns
 monday_ratings = monday_ratings[['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4']]
+# remove NaN values
 monday_ratings = monday_ratings.dropna()
 
-
-
-# print(monday_ratings.iloc[4:-2,2:8])
 print(monday_ratings)
 
-# need to set column titles
+# need to set column titles from 'Unnamed : _' to ['Program', 'QH Rtg', 'QH Share']
 # find more xlsx from Mondays near that date and download
-# tell script to fill the NaN values on TP Sh (or maybe omit???)
-
-
-
